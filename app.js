582ee2f0f5235cf12f1ba8f091d43930bbbc0336
Max <melihov89@gmail.com>
2023-07-29 20:12:44 +0300

    fff

--- app.js
+++ app.js
@@ -2,26 +2,25 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 
-const girl1 = document.getElementById('girl1');
-const girl2 = document.getElementById('girl2');
+
 const like = document.getElementById('like');
+const carousel = document.getElementById('carouselExampleControls');
+carousel.addEventListener('slid.bs.carousel', getActiveSlideId);
 
 
-// Add an event listener to the carousel when it slides
-$('.carousel').on('slid.bs.carousel', function () {
-  // Get the active slide using the "active" class
-  const activeSlide = $(this).find('.carousel-item.active');
-  // Get the index of the active slide
-  const activeSlideIndex = $(activeSlide).index();
-  
-  like.addEventListener('click', sendData);
-  // The function that handles the click event
-  function sendData() {
-    tg.sendData(activeSlideIndex.toString());
-  }
 
-  // Output the active slide index to the console or use it as needed
-  console.log('Active slide ID:', activeSlideIndex);
-  console.log('item is now:',activeSlideIndex);
-});
+
+function getActiveSlideId() {
+    let activeSlide = document.querySelector('.carousel-item.active');
+    let activeSlideId = activeSlide.id;
+    return activeSlideId.toString();
+}
+
+like.addEventListener('click', showLog);
+
+    function showLog() {
+        tg.sendData(getActiveSlideId());
+    }
+
+
 
