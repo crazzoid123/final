754742313a238c3488ce1a290e1a7a63eb2564bc
Max <melihov89@gmail.com>
2023-07-29 19:48:16 +0300

    senddata

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ tg.expand();
 
 const girl1 = document.getElementById('girl1');
 const girl2 = document.getElementById('girl2');
+const like = document.getElementById('like');
 
 
 // Add an event listener to the carousel when it slides
@@ -12,8 +13,12 @@ $('.carousel').on('slid.bs.carousel', function () {
   const activeSlide = $(this).find('.carousel-item.active');
   // Get the index of the active slide
   const activeSlideIndex = $(activeSlide).index();
-
+  
+  like.addEventListener('click', sendData);
   // The function that handles the click event
+  function sendData() {
+    tg.sendData(activeSlideIndex.toString());
+  }
 
   // Output the active slide index to the console or use it as needed
   console.log('Active slide ID:', activeSlideIndex);
